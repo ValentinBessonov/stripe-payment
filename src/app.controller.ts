e0478764edda147cdeee267ad443052216fcd60b
Bessonov Valentin <valentbesson@gmail.com>
2023-03-15 22:31:08 +0400

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,20 @@
-import { HttpService } from '@nestjs/axios/dist';
 import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller("api")
 export class AppController {
   private stripe;
 
-  constructor(private readonly httpService: HttpService) {
+  constructor() {
     this.stripe = require("stripe")("sk_test_51MlrevGxkAEWJXFvFvAmwavTSDcChcWBylcFnieVbF2BLfZmd9qi1TYCUnFxH11ct14ODPKmfDmIrc9o7Kt0aenk00zjSjJ7Ld");
   }
 
-  @Get("token")
-  async CreateToken() {
-    const token = await this.stripe.terminal.connectionTokens.create();
-    return token
-
+  @Get("connection_token")
+  async ConnectionToken() {
+    const connectionToken = await this.stripe.terminal.connectionTokens.create();
+    return { secret: connectionToken.secret }
   }
 
-  @Post("create-location")
+  @Post("create_location")
   async CreateLocation() {
     const location = await this.stripe.terminal.locations.create({
       display_name: 'HQ',
@@ -31,5 +28,55 @@ export class AppController {
     });
 
     return location;
+  }
+
+  @Post("register_reader")
+  async RegisterReader(req) {
+    const reader = await this.stripe.terminal.readers.create({
+      registration_code: 'simulated-wpe',
+      location: req.body.location_id,
+    });
+
+    return reader;
+  }
+
+  @Post("create_payment_intent")
+  // For Terminal payments, the 'payment_method_types' parameter must include
+  // 'card_present'.
+  // To automatically capture funds when a charge is authorized,
+  // set `capture_method` to `automatic`.
+  async CreatePaymentIntent(req) {
+    const intent = await this.stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      payment_method_types: [
+        'card_present',
+      ],
+      capture_method: 'manual',
+    });
+
+    return intent;
+  }
+
+  @Post("process_payment")
+  async ProcessPayment(req) {
+    const reader = await this.stripe.terminal.readers.processPaymentIntent(req.body.reader_id, {
+      payment_intent: req.body.payment_intent_id
+    });
+
+    return reader;
+  }
+
+  @Post("simulate_payment")
+  async SimulatePayment(req) {
+    const reader = await this.stripe.testHelpers.terminal.readers.presentPaymentMethod(req.body.reader_id);
+
+    return reader;
+  }
+
+  @Post("capture_payment_intent")
+  async CapturePaymentIntent(req) {
+    const intent = await this.stripe.paymentIntents.capture(req.body.payment_intent_id);
+    return intent;
   }
 }
